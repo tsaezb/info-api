5e1fb7ff0e5c611e10f9fdbd1521a662d15da853
tsaezb <tomas.saezbin@gmail.com>
2017-09-08 16:31:17 -0300

    add serializares, views & urls

--- infobox/serializers.py
+++ infobox/serializers.py
@@ -1,13 +1,14 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
+from infobox.models import Property
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email')
+        fields = ('url', 'username', 'email', 'groups')
 
-class PropertiesSerializer(serializers.HyperlinkedModelSerializer):
+class PropertySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Property
-        fields = ('name', 'id', 'frecuency')
+        fields = ('label', 'identifier', 'frecuency')
--- infobox/views.py
+++ infobox/views.py
@@ -2,5 +2,23 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from infobox.models import Property
+from rest_framework import viewsets
+from infobox.serializers import UserSerializer, PropertySerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class PropertyViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows properties to be viewed or edited.
+    """
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
