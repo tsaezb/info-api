56ad39048c38bc4e59dbeaa673821669cfa0c7f2
Tomas Andres Saez Binelli <tsaez@arol>
2018-01-18 14:57:03 -0300

    heroku test

--- Infoapi/settings.py
+++ Infoapi/settings.py
@@ -91,12 +91,22 @@ WSGI_APPLICATION = 'Infoapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'on_heroku' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.config()
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        } 
+    }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
